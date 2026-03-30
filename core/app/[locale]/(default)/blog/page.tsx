f59c3858ed9a2fb83332eaa825161135b0651b7d
Mark H <markh@cast-lighting.com>
2026-03-29 23:45:57 -0400

    fix: disable CmsPageRenderer on blog page until /blog Makeswift page is created (was returning empty snapshot)

--- core/app/[locale]/(default)/blog/page.tsx
+++ core/app/[locale]/(default)/blog/page.tsx
@@ -49,8 +49,9 @@ export default async function Blog(props: Props) {
   const { locale } = await props.params;
   setRequestLocale(locale);
 
-  const makeswiftPage = await CmsPageRenderer({ templatePath: '/blog', data: {} });
-  if (makeswiftPage) return makeswiftPage;
+  // Uncomment when /blog page is created in Makeswift:
+  // const makeswiftPage = await CmsPageRenderer({ templatePath: '/blog', data: {} });
+  // if (makeswiftPage) return makeswiftPage;
 
   const searchParamsParsed = searchParamsCache.parse(await props.searchParams);
   const { tag, before, after, limit } = searchParamsParsed;
