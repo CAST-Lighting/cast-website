97fe9230a6dc3d248b700b08ec3679a2314cd63e
CAST-Lighting <markh@cast-lighting.com>
2026-04-18 13:34:43 -0400

    fix: syntax error in NewsletterCta.tsx - restore missing commas

--- core/lib/makeswift/components/cast/NewsletterCta.tsx
+++ core/lib/makeswift/components/cast/NewsletterCta.tsx
@@ -12,7 +12,7 @@ const NewsletterCta = forwardRef(function NewsletterCta(
     gradientFrom,
     gradientTo,
     gradientDirection,
-    lineHeight
+    lineHeight,
     heading,
     headingAccent,
     description,
