6175c0bd66aae3b718db000e6e9dc50a9b4f3aa9
VAVA Graphics <tristan@vavagraphics.com>
2026-04-20 04:15:05 -0400

    fix: correct StyleGuide.register.ts syntax again after restore

--- core/lib/makeswift/components/cast/StyleGuide.register.ts
+++ core/lib/makeswift/components/cast/StyleGuide.register.ts
@@ -1,5 +1,5 @@
 import { lazy } from "react"
-import { Style, Checkbox} from "@makeswift/runtime/controls"
+import { Style } from "@makeswift/runtime/controls"
 import { runtime } from "~/lib/makeswift/runtime"
 
 runtime.registerComponent(
@@ -8,6 +8,7 @@ runtime.registerComponent(
     type: "cast-style-guide",
     label: "Utilities / Style Guide",
     props: {
-      className: Style({ properties: [Style.Padding, Style.Margin] }) } }
-      lightMode: Checkbox({ label: '💡 Light Mode', defaultValue: false }),
+      className: Style({ properties: [Style.Padding, Style.Margin] }),
+    },
+  }
 )
